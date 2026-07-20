362ca2b0f57ab2db59fa7af7366610190fa05967
Nuranga <kaushalyanuranga@gmail.com>
2026-07-20 14:41:52 +0530

    feat: add simulated live delivery stats to landing page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import AtrForm from './AtrForm';
 import AdminDashboard from './AdminDashboard';
 import StaffDashboard from './StaffDashboard';
 import Chatbot from './Chatbot';
+import StatsSection from './StatsSection';
 
 const hashPassword = async (password) => {
     const encoder = new TextEncoder();
@@ -789,6 +790,7 @@ function App({ onNavigate }) {
                                 <>
                                     <h1>Track Your <br /><span className="highlight">Delivery</span> Instantly</h1>
                                     <p>Enter your tracking number below to get real-time updates on your package location and delivery status.</p>
+                                    <StatsSection />
                                 </>
                             )}
                         </div>
